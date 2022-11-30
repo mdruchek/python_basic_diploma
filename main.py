3653fe5a1105a67dd56a5a2a4b77f9b0a9707467
Дручек Михаил <druchek_ma@vsw.ru>
2022-11-30 18:42:08 +0300

    modified main.py

--- main.py
+++ main.py
@@ -1,35 +1,58 @@
-import lowprice
 import modules
 import telebot
 
 
-
-token: str = modules.get_config_from_file(path='./config.ini', section='account', setting='token_bot')
+token: str = modules.get_config_from_file(path='./config.ini', section='account', setting='token')
 my_bot: telebot.TeleBot = telebot.TeleBot(token)
 
-@my_bot.message_handler(commands=['help', 'lowprice'])
-def get_text_messages(message) -> None:
+
+@my_bot.message_handler(commands=['lowprice'])
+def lowprice_command(message) -> None:
     """
-    Функция обмена текстовыми сообщениями Телеграмм-бота
+    Функция для вывода самых дешёвых отелей
     """
-    if message.text == "/help":
-        my_bot.send_message(message.from_user.id,
-                          "Привет меня зовут MDr, я могу подобрать отель для проживания.\n"
-                          "Я понимаю такие команды:\n"
-                          "/lowprice - вывод самых дешёвых отелей в городе\n"
-                          "/highprice - вывод самых дорогих отелей в городе\n"
-                          "/bestdeal - вывод отелей, наиболее подходящих по цене и расположению от центра\n"
-                          "/history - вывод истории поиска отелей\n")
-    elif message.text == '/lowprice':
-        my_bot.send_message(message.from_user.id, 'Функция в разработке.')
-    elif message.text == '/highprice':
-        my_bot.send_message(message.from_user.id, 'Функция в разработке.')
-    elif message.text == '/bestdeal':
-        my_bot.send_message(message.from_user.id, 'Функция в разработке.')
-    elif message.text == '/history':
-        my_bot.send_message(message.from_user.id, 'Функция в разработке.')
-    else:
-        my_bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+
+@my_bot.message_handler(commands=['highprice'])
+def highprice_command(message) -> None:
+    """
+    Функция для вывода самых дорогих отелей
+    """
+
+
+@my_bot.message_handler(commands=['bestdeal'])
+def bestdeal_command(message) -> None:
+    """
+    Функция для вывода отелей, наиболее подходящих по цене и расположению от центра
+    """
+
+
+@my_bot.message_handler(commands=['history'])
+def history_command(message) -> None:
+    """
+    Функция для вывода вывод истории поиска отелей
+    """
+
+
+@my_bot.message_handler(commands=['help'])
+def help_command(message) -> None:
+    """
+    Функция для вывода справки по функциям
+    """
+    my_bot.send_message(message.from_user.id, 'Привет меня зовут MDr, я могу подобрать отель для проживания.\n'
+                                              'Я понимаю такие команды:\n'
+                                              '/lowprice - вывод самых дешёвых отелей в городе\n'
+                                              '/highprice - вывод самых дорогих отелей в городе\n'
+                                              '/bestdeal - вывод отелей, наиболее подходящих по цене и расположению от центра\n'
+                                              '/history - вывод истории поиска отелей')
+
+
+@my_bot.message_handler(func=lambda text: True)
+def any_text(message) -> None:
+    """
+    Функция обработки остального текста
+    """
+    my_bot.send_message(message.from_user.id, 'Я Вас не понимаю, введите /help')
 
 
 my_bot.polling(none_stop=True, interval=0)
