565a16fdb4ea68c6baa7d2eeb0de2429ee774020
Дручек Михаил <druchek_ma@vsw.ru>
2022-11-30 22:59:50 +0300

    add lowprice.py

---
+++ lowprice.py
@@ -0,0 +1,26 @@
+import json
+import requests
+import modules
+
+
+class Lowprice:
+	"""
+	Класс содержит методы для функции lowprice телеграмм бота
+	"""
+	@classmethod
+	def get_location_city(cls, city):
+		Lowprice.city = 'new york'
+		url = "https://hotels4.p.rapidapi.com/locations/v3/search"
+
+		querystring = {"q": city}
+
+		x_rapid_apy_key = modules.get_config_from_file(path='./config.ini', section='account', setting='X-RapidAPI-Key')
+
+		headers = {
+			"X-RapidAPI-Key": x_rapid_apy_key,
+			"X-RapidAPI-Host": "hotels4.p.rapidapi.com"
+		}
+
+		response = requests.request("GET", url, headers=headers, params=querystring)
+		return response
+
