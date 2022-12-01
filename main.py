ac2d2d9af186e66f02028dc7e1a3455d04960cbe
Mihail Druchek <druchek_ma@vsw.ru>
2022-12-01 09:43:09 +0300

    changed the methods of responding to messages

--- main.py
+++ main.py
@@ -15,8 +15,7 @@ def telegram_bot(token):
         """
         telegram_bot.command = message.text
         telegram_bot.command_message_id = message.id
-        my_bot.send_message(message.from_user.id, "В каком городе ищем?")
-
+        my_bot.reply_to(message, "В каком городе ищем?")
 
     @my_bot.message_handler(commands=['highprice'])
     def highprice_command(message) -> None:
@@ -25,9 +24,7 @@ def telegram_bot(token):
         """
         telegram_bot.command_message_id = message.id
         telegram_bot.command = message.text
-        my_bot.send_message(message.from_user.id, "В каком городе ищем?")
-
-
+        my_bot.reply_to(message, "В каком городе ищем?")
 
     @my_bot.message_handler(commands=['bestdeal'])
     def bestdeal_command(message) -> None:
@@ -36,8 +33,7 @@ def telegram_bot(token):
         """
         telegram_bot.command_message_id = message.id
         telegram_bot.command = message.text
-        my_bot.send_message(message.from_user.id, "В каком городе ищем?")
-
+        my_bot.reply_to(message, "В каком городе ищем?")
 
     @my_bot.message_handler(commands=['history'])
     def history_command(message) -> None:
@@ -46,21 +42,19 @@ def telegram_bot(token):
         """
         telegram_bot.command_message_id = message.id
         telegram_bot.command = message.text
-        my_bot.send_message(message.from_user.id, "В каком городе ищем?")
-
+        my_bot.reply_to(message.from_user.id, "В каком городе ищем?")
 
     @my_bot.message_handler(commands=['help'])
     def help_command(message) -> None:
         """
         Функция для вывода справки по функциям
         """
-        my_bot.send_message(message.from_user.id, 'Привет меня зовут MDr, я могу подобрать отель для проживания.\n'
-                                                  'Я понимаю такие команды:\n'
-                                                  '/lowprice - вывод самых дешёвых отелей в городе\n'
-                                                  '/highprice - вывод самых дорогих отелей в городе\n'
-                                                  '/bestdeal - вывод отелей, наиболее подходящих по цене и расположению от центра\n'
-                                                  '/history - вывод истории поиска отелей')
-
+        my_bot.reply_to(message, 'Привет меня зовут MDr, я могу подобрать отель для проживания.\n'
+                                 'Я понимаю такие команды:\n'
+                                 '/lowprice - вывод самых дешёвых отелей в городе\n'
+                                 '/highprice - вывод самых дорогих отелей в городе\n'
+                                 '/bestdeal - вывод отелей, наиболее подходящих по цене и расположению от центра\n'
+                                 '/history - вывод истории поиска отелей')
 
     @my_bot.message_handler(func=lambda text: True)
     def any_text(message) -> None:
@@ -69,14 +63,13 @@ def telegram_bot(token):
         """
         if message.id == telegram_bot.command_message_id + 2:
             if telegram_bot.command == '/lowprice':
-                my_bot.send_message(message.from_user.id, lowprice.Lowprice.get_location_city(message.text))
+                my_bot.reply_to(message, lowprice.Lowprice.get_location_city(message.text))
             else:
-                my_bot.send_message(message.from_user.id, 'функция в разработке')
+                my_bot.reply_to(message, 'функция в разработке')
         else:
-            my_bot.send_message(message.from_user.id, 'Я Вас не понимаю, введите /help')
+            my_bot.reply_to(message, 'Я Вас не понимаю, введите /help')
 
-
-    my_bot.polling(none_stop=True, interval=0)
+    my_bot.infinity_poling()
 
 
 if __name__ == '__main__':
