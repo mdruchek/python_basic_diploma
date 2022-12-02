a66da0c7ce2b08fd27f5c1e0311052533601df83
Mihail Druchek <druchek_ma@vsw.ru>
2022-12-02 08:46:53 +0300

    removed data storage in a file, add data storage in a class

--- modules.py
+++ modules.py
@@ -1,7 +1,5 @@
 from configparser import ConfigParser
-import shelve
-import os
-from typing import Dict
+from typing import List
 
 
 def get_config_from_file(path: str, section: str, setting: str):
@@ -37,53 +35,90 @@ def get_config_from_file(path: str, section: str, setting: str):
     return setting_in_file
 
 
-class DataToShelve:
+class UserSurvey:
     """
-    Класс для записи данных в файл с помощью модуля shelve
+    Класс опрос пользователя
     """
-    @classmethod
-    def adding_data_to_shelve(cls, path: str, key: str, data: str) -> None:
-        """
-        Метод записи данных
+    __questions = ('В каком городе ищем?',
+                 'В каком диапазоне цен искать?',
+                 'Какое расстояние от центра Вас устроит?',
+                 'Сколько отелей вывести?',
+                 'Загрузить фотографи?',
+                 'Сколько фотографий загрузить?')
 
-        :param path: имя файла
-        :type path: str
+    __survey_list = {'/lowprice': [0, 3, 4],
+                   '/highprice': [0, 3, 4],
+                   '/bestdeal': [0, 1, 2, 3, 4]}
 
-        :param key: ключ
-        :type key: str
+    def __init__(self, command):
+        self.__command: str = command
+        self.__command_number = 0
+        self.__city: str = 'None'
+        self.__price: List = [None, None]
+        self.__distance: List = [None, None]
+        self.__number_hotels: int = 0
+        self.__uploading_photos: bool = False
+        self.__number_photos: int = 0
 
-        :param data: данные
-        :type data: str
-        """
-        shelve_buffer = shelve.open(path)
-        shelve_buffer[key] = data
-        shelve_buffer.close()
+    @property
+    def command(self):
+        return self.__command
 
-    @classmethod
-    def read_data_from_shelve(cls, path: str, key: str) -> Dict:
-        """
-        Метод чтения данных
+    @property
+    def city(self):
+        return self.__city
 
-        :param path: имя файла
-        :type path: str
+    @property
+    def price(self):
+        return self.__price
 
-        :param key: ключ
-        :type key: str
-        """
-        shelve_buffer = shelve.open(path)
-        data = shelve_buffer[key]
-        shelve_buffer.close()
-        return {key: data}
+    @property
+    def distance(self):
+        return self.__distance
 
-    @classmethod
-    def remove_shelve(cls, path: str) -> None:
-        """
-        Метод удаления файла данных
+    @property
+    def number_hotels(self):
+        return self.__number_hotels
 
-        :param path: имя файла
-        :type path: str
-        """
-        if os.path.exists(path):
-            os.remove(path)
-        else:
-            raise FileExistsError('Файл {file} не существует.'.format(file=path))
+    @property
+    def uploading_photos(self):
+        return self.__uploading_photos
+
+    @property
+    def number_photos(self):
+        return self.__number_photos
+
+    @command.setter
+    def command(self, command):
+        self.__command = command
+
+    @city.setter
+    def city(self, city):
+        self.__city = city
+
+    @price.setter
+    def price(self, price):
+        self.__price = price
+
+    @distance.setter
+    def distance(self, distance):
+        self.__distance = distance
+
+    @number_hotels.setter
+    def number_hotels(self, number_hotels):
+        self.__number_hotels = number_hotels
+
+    @uploading_photos.setter
+    def uploading_photos(self, uploading_photos):
+        self.__uploading_photos = uploading_photos
+
+    @number_photos.setter
+    def number_photos(self, number_photos):
+        self.__number_photos = number_photos
+
+    def get_question(self):
+        if self.__command_number > len(UserSurvey.__survey_list[self.__command]):
+            return False
+        question = UserSurvey.__questions[UserSurvey.__survey_list[self.__command_number]]
+        self.__command_number += 1
+        return question
