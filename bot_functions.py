c2b0c4c1e36a7ceda2027f666e30c74c93a8d619
Mihail Druchek <druchek_ma@vsw.ru>
2022-12-08 11:46:20 +0300

    bot_functions.py: deleted the requests class, created the botfunctions class

--- bot_functions.py
+++ bot_functions.py
@@ -1,21 +1,8 @@
 import modules
 
 
-class Lowprice:
+class BotFunctions:
 	"""
-	Класс содержит методы для функции lowprice телеграмм бота
+	Класс содержит методы для реализации функций телеграмм бота
 	"""
 
-	__found_hotels_list = []
-
-	@property
-	def found_hotels_list(self):
-		return Lowprice.__found_hotels_list
-
-	@classmethod
-	def get_properties_list(cls, city, result_size):
-		Lowprice.__found_hotels = modules.Requests.properties_list(city=city,
-																   check_in_date=None,
-																   check_out_date=None,
-																   result_size=result_size,
-																   sort='PRICE_LOW_TO_HIGH')
